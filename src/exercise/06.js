8d6804855ac158b26926a6b2cdb93ed404a1f0da
Whitley Horn <whitleyblaine@protonmail.com>
2022-03-17 16:22:29 -0400

    Finish extra credit

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -59,6 +59,7 @@ function App() {
         <ErrorBoundary
           FallbackComponent={ErrorFallback}
           onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
         >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
